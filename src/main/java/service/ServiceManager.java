ec2ae24aa58119436aafe2f68e6403eaf0837784
”Opanasenko-Mykhailo <opanasenko.misha@gmail.com>
2024-02-18 22:43:05 +0200

    update

--- src/main/java/service/ServiceManager.java
+++ src/main/java/service/ServiceManager.java
@@ -66,7 +66,7 @@ public class ServiceManager {
             film.setRating(Rating.PG13);
             film.setSpecialFeatures(Set.of(Feature.TRAILERS, Feature.BEHIND_THE_SCENES));
             film.setLength((short) 22);
-            film.setReplacementCost(BigDecimal.valueOf(2));
+            film.setReplacementCost(BigDecimal.ONE);
             film.setRentalRate(BigDecimal.ZERO);
             film.setLanguage(language);
             film.setDescription("new scary film");
